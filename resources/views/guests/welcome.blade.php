274e88a01b00a3217091911f0bd00bb7abf6727d
giacomomarongiu <giacomomarongiu1993@gmail.com>
2024-05-09 17:41:33 +0200

    [Fix]

--- resources/views/guests/welcome.blade.php
+++ resources/views/guests/welcome.blade.php
@@ -27,7 +27,7 @@
                 <div class="row g-3">
                     @foreach ($movies as $movie)
                         <div class="col">
-                            <div class="card border border-danger" style="width: 18rem;">
+                            <div class="card border border-danger m-auto" style="width: 18rem;">
                                 <img src="https://picsum.photos/200/200" class="card-img-top" alt="...">
                                 <div class="card-body">
                                     <h5 class="card-title">{{ $movie->title }}</h5>
