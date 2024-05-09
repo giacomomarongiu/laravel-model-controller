7c2c134353332b18af992b429f9b1004c2f80590
giacomomarongiu <giacomomarongiu1993@gmail.com>
2024-05-09 15:25:32 +0200

    [Init] Start repo + First structure

---
+++ app/Http/Controllers/Guests/PageController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Guests;
+
+use App\Http\Controllers\Controller;
+use App\Models\Movie;
+//use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function index()
+    {
+        $movies = Movie::all();
+         //dd($movies);
+        return view('welcome', compact('movies'));
+    }
+}
